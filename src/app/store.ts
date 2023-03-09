0d6f123a802f6dd6d6ebc5d79056de8083eb7798
Mihail Schutow <schutow.mihail@yandex.ru>
2023-03-09 21:07:38 +0300

    Feat localStorage data saving

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,11 +2,35 @@ import { combineReducers, legacy_createStore as createStore } from 'redux';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import { notesReducer, NotesReducerActionTypes } from '../modules/noteList/notesReducer';
 
+const saveToLocalStorage = (state: AppRootStateType) => {
+    try {
+        localStorage.setItem('state', JSON.stringify(state));
+    } catch (e) {
+        console.error(e);
+    }
+};
+
+const loadFromLocalStorage = () => {
+    try {
+        const stateStr = localStorage.getItem('state');
+        return stateStr ? JSON.parse(stateStr) : undefined;
+    } catch (e) {
+        console.error(e);
+        return undefined;
+    }
+};
+
+const persistedStore = loadFromLocalStorage();
+
 const rootReducer = combineReducers({
     notesData: notesReducer,
 })
 
-export const store = createStore(rootReducer);
+export const store = createStore(rootReducer, persistedStore);
+
+store.subscribe(() => {
+    saveToLocalStorage(store.getState());
+});
 
 export type AppRootStateType = ReturnType<typeof rootReducer>
 export type CommonActionsType = NotesReducerActionTypes
